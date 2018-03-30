43ee62c2dba894bdf0d0d71c8187a857e46e4c53
Jay McCarthy <jay.mccarthy@gmail.com>
2018-03-30 19:49:01 -0400

    init, doesnt work

---
+++ info.rkt
@@ -0,0 +1,10 @@
+#lang info
+(define collection "drracket-autorun")
+(define deps '("base"))
+(define build-deps '())
+(define version "0.1")
+(define pkg-authors '(jeapostrophe))
+
+(define drracket-tools '(("tool.rkt")))
+(define drracket-tool-names (list "Autorun"))
+(define drracket-tool-icons '(#f))
---
+++ tool.rkt
@@ -0,0 +1,58 @@
+#lang racket/base
+(require racket/unit
+         drracket/tool
+         racket/gui
+         mrlib/switchable-button
+         racket/class
+         images/icons/style
+         images/icons/symbol)
+
+(define tool@
+  (unit
+    (import drracket:tool^)
+    (export drracket:tool-exports^)
+
+    (define (phase1) (void))
+    (define (phase2) (void))
+
+    (define autorun-mixin
+      (mixin ((class->interface text%)) ()
+
+        (define/augment (after-edit-sequence)
+          (when autorun?
+            (send autorun? execute-callback)))
+        
+        (define autorun? #f)
+        (define/public (flip-autorun! the-unit-frame)
+          (set! autorun? (if autorun? #f the-unit-frame)))
+
+        (super-new)))
+
+    (define autorun-button-mixin
+      (mixin (drracket:unit:frame<%>) ()
+        (super-new)
+        (inherit get-button-panel
+                 get-definitions-text)
+        (inherit register-toolbar-button)
+
+        (let ()
+          (define autorun-icon
+            (recycle-icon #:color run-icon-color))
+          (define btn
+            (new switchable-button%
+                 (label "Autorun")
+                 (callback (λ (button)
+                             (send (get-definitions-text)
+                                   flip-autorun!
+                                   this)))
+                 (parent (get-button-panel))
+                 (bitmap autorun-icon)))
+          (register-toolbar-button btn #:number 11)
+          (send (get-button-panel) change-children
+                (λ (l)
+                  (cons btn (remq btn l)))))))
+
+    (drracket:get/extend:extend-definitions-text autorun-mixin)
+    (drracket:get/extend:extend-unit-frame autorun-button-mixin)))
+
+(provide tool@)
